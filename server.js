c70e83cd3239b7c62d837dde8348777acfe12d3a
Aleksei Kosumov <AKosumovJob@gmail.com>
2023-04-30 03:24:40 +0300

    1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const db = require('./config/connection')
 const routes = require('./routes')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -8,6 +9,11 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(routes)
+app.use(
+	cors({
+		origin: ['*'],
+	})
+)
 
 db.once('open', () => {
 	app.listen(PORT, () => {
