faf8c43be6a7ff0e413c351b240a27b012749c4f
Aleksei Kosumov <AKosumovJob@gmail.com>
2023-04-30 03:51:17 +0300

    1

--- server.js
+++ server.js
@@ -9,17 +9,11 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(routes)
-
-app.get('/api/v1/form', (req, res) => {
-	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	)
-	res.end()
-})
-
+app.use(
+	cors({
+		origin: ['https://kosnet-production.up.railway.app'],
+	})
+)
 db.once('open', () => {
 	app.listen(PORT, () => {
 		console.log(`API server running on port ${PORT}!`)
