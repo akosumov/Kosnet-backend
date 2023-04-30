51134ed60fcd69763c6bbc1c01553573f6f6d5dd
Aleksei Kosumov <AKosumovJob@gmail.com>
2023-04-30 16:12:26 +0300

    1

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(
-	cors({
-		credentials: true,
-	})
-)
+app.use(cors())
 
 app.use(routes)
 
