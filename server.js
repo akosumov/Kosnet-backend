f99ef1f13fa47fa6889fd79c5403ab4ec62e6bd3
Aleksei Kosumov <AKosumovJob@gmail.com>
2023-04-30 05:28:47 +0300

    1

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(routes)
 app.use(cors())
+app.use(routes)
 db.once('open', () => {
 	app.listen(PORT, () => {
 		console.log(`API server running on port ${PORT}!`)
