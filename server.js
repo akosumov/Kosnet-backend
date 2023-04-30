ab58321ef0239a7dface3d4f5fed2cb7d29a58ef
Aleksei Kosumov <AKosumovJob@gmail.com>
2023-04-30 03:42:44 +0300

    1

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ const cors = require('cors')
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.use(
-	cors({
-		origin: ['https://kosnet-production.up.railway.app/api/users'],
-	})
-)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(routes)
+
+app.get('/api/v1/form', (req, res) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	)
+	res.end()
+})
 
 db.once('open', () => {
 	app.listen(PORT, () => {
