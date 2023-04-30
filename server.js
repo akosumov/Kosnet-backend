05120c1b7f81d59f998a2e8ff03e9d59e284fd98
Aleksei Kosumov <AKosumovJob@gmail.com>
2023-04-30 03:36:38 +0300

    1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(routes)
 app.use(
 	cors({
-		origin: ['https://kosnet-production.up.railway.app'],
+		origin: ['https://kosnet-production.up.railway.app/api/users'],
 	})
 )
 
