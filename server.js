c47f7a23dd891975371efd76c51b770c617d9475
Aleksei Kosumov <AKosumovJob@gmail.com>
2023-04-30 03:53:23 +0300

    1

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(routes)
-app.use(
-	cors({
-		origin: ['https://kosnet-production.up.railway.app'],
-	})
-)
+app.use(cors())
 db.once('open', () => {
 	app.listen(PORT, () => {
 		console.log(`API server running on port ${PORT}!`)
